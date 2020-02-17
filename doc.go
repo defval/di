c7eb1f39bee5cd8e41fe6af19ac40c95da5ef45b
Maxim Bovtunov <dev@defval.com>
2020-02-17 22:58:23 +0300

    Fix license

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
-// The MIT License (MIT)
+// MIT License
 //
-// Copyright (c) 2019 defval
+// Copyright (c) 2020 defval
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
